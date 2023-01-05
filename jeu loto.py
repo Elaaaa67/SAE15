93eddd8165b89392b8ac792d5ed3bead7deac5cf
Elaaaa67 <ela.blt2767@gmail.com>
2023-01-05 11:32:18 +0100

    v.3

--- jeu loto.py
+++ jeu loto.py
@@ -1,7 +1,14 @@
-import random
+
+l1 =[]
+x = 0
+
+if x< 5 :
+    n = (int(input("Entrez un nombre entre 1 et 45:")))
+    for i in range (l1):
+    x = x+1
+else x > 5
+    print("voici vos nombre:"{l1})
 
 from numpy.random import randint
 print(randint(1,45,5))
-random.seed(1)
 
-#n = (int(input("Entrez un nombre entre 1 et 45:"))).
